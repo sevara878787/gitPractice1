c8346661340c6134b9d98e8e6c678e7185f998ac
Sevara Yuldasheva <sevara878787@mail.ru>
2020-11-12 16:54:05 -0500

    add Login.java

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -4,6 +4,6 @@ public class HelloGitWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello dear code");
+        System.out.println("Hello dear code ");
     }
 }
---
+++ src/tests/Login.java
@@ -0,0 +1,16 @@
+package tests;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        boolean result = true;
+
+        if(result){
+            System.out.println("Login is successfull");
+        }else {
+            System.out.println("Login is not successful");
+        }
+    }
+
+}
